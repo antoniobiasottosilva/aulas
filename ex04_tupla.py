946a6094443a587fd922c59d39b008aea383de07
antoniobiasottosilva <antonio.biasotto.silva@escola.pr.gov.br>
2026-06-07 18:56:57 -0300

    Delete ex04_tupla.py

--- ex04_tupla.py
+++
@@ -1,9 +0,0 @@
-temperaturas = (36.5, 37.2, 38.0, 36.8, 39.1)
-
-for temp in temperaturas:
-    if temp < 37.5:
-        print(f"{temp}°C: Normal")
-    elif 37.5 <= temp <= 38.5:
-        print(f"{temp}°C: Febre moderada")
-    else:
-        print(f"{temp}°C: Febre alta")
